5534578059dadac21211d5109fb3dc8fa257cbe1
satomiryumae <70643704+satomiryumae@users.noreply.github.com>
2020-10-04 21:28:18 +0900

    Initial commit

---
+++ Assets/scripts/ColliderManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderManager : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
